44d9d5eb5c01b387cc287af9f3a5dea6b644f3a7
Barbara Insaurralde <40776592+barbiagus22@users.noreply.github.com>
2018-09-08 13:05:31 -0300

    Ej 58- Guia

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int vector [10]= {0,1,2,3,4,5,6,7,8,9};
+
+    for (int i=0; i<10 ; i++)
+    {
+        printf(" Los numero del vector: %d \n ", vector[i]);
+    }
+
+    return 0;
+}
